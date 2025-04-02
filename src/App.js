d4d78b23a739a0aa03460259e32b6a615d567e3a
luiscadari <luiscadari@icloud.com>
2025-04-01 21:36:42 -0300

    :lipstick: feat: pallete collor

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
+import React from "react";
 import "./App.css";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello World</h1>
     </div>
   );
 }
-
-export default App;
